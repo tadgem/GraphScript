722195862c4db85f21ba61bba4e20e2da168ef96
tadgem <98434757+tadgem@users.noreply.github.com>
2024-02-21 23:38:22 +0000

    update a bit

--- editor/src/GraphScriptEditor.cpp
+++ editor/src/GraphScriptEditor.cpp
@@ -9,49 +9,38 @@ gs::GraphScriptEditor::GraphScriptEditor(GraphBuilder* builder)
 void gs::GraphScriptEditor::OnImGui()
 {
 	ImNodes::BeginNodeEditor();
-
 	for (Node* node : m_Builder->m_Nodes)
 	{
-		ImNodes::BeginNode((int)node);
+		ImNodes::BeginNode(node->m_NodeName.m_Value);
 		ImNodes::BeginNodeTitleBar();
 		ImGui::Text(node->m_NodeName.m_Original.c_str());
 		ImNodes::EndNodeTitleBar();
 		for (ExecutionSocket* execution : node->m_InputExecutionSockets)
 		{
-			ImNodes::BeginInputAttribute((int)execution, ImNodesPinShape_Triangle);
+			ImNodes::BeginInputAttribute(execution->m_SocketName.m_Value, ImNodesPinShape_Triangle);
 			ImGui::Text(execution->m_SocketName.m_Original.c_str());
 			ImNodes::EndInputAttribute();
 		}
 		for (auto [name, socket] : node->m_InputDataSockets)
 		{
-			ImNodes::BeginInputAttribute((int)socket, ImNodesPinShape_QuadFilled);
+			ImNodes::BeginInputAttribute(name.m_Value, ImNodesPinShape_QuadFilled);
 			ImGui::Text(name.m_Original.c_str());
 			ImNodes::EndInputAttribute();
 		}
 		for (ExecutionSocket* execution : node->m_OutputExecutionSockets)
 		{
-			ImNodes::BeginOutputAttribute((int)execution, ImNodesPinShape_Triangle);
+			ImNodes::BeginOutputAttribute(execution->m_SocketName.m_Value, ImNodesPinShape_Triangle);
 			ImGui::Text(execution->m_SocketName.m_Original.c_str());
 			ImNodes::EndOutputAttribute();
 		}
 		for (auto [name, socket] : node->m_OutputDataSockets)
 		{
-			ImNodes::BeginOutputAttribute((int)socket, ImNodesPinShape_QuadFilled);
+			ImNodes::BeginOutputAttribute(name.m_Value, ImNodesPinShape_QuadFilled);
 			ImGui::Text(name.m_Original.c_str());
 			ImNodes::EndOutputAttribute();
 		}
 		ImNodes::EndNode();
 	}
-	/*
-	for (auto dataConn : m_Builder->m_DataConnections)
-	{
-		ImNodes::Link((int)dataConn, (int)dataConn->m_LHS, (int)dataConn->m_RHS);
-	}
 
-	for (auto exeConn : m_Builder->m_ExecutionConnections)
-	{
-		ImNodes::Link((int)&exeConn, (int)exeConn.m_LHS, (int)exeConn.m_RHS);
-	}
-	*/
 	ImNodes::EndNodeEditor();
 }
