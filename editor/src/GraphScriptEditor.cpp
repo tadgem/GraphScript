c7331a65f883178b4dd6a8b32ff0efa9ec074247
tadgem <98434757+tadgem@users.noreply.github.com>
2024-02-22 13:24:27 +0000

    more ui update i hate this

--- editor/src/GraphScriptEditor.cpp
+++ editor/src/GraphScriptEditor.cpp
@@ -8,38 +8,78 @@ gs::GraphScriptEditor::GraphScriptEditor(GraphBuilder* builder)
 
 void gs::GraphScriptEditor::OnImGui()
 {
+	int idCounter = 1;
+	HashMap<void*, int> counterMap;
+
 	ImNodes::BeginNodeEditor();
+	for (auto& [name, var] : m_Builder->m_VariablesDefs)
+	{
+		ImNodes::BeginNode(idCounter);
+		counterMap.emplace(var.get()->GetSocket(), idCounter);
+		idCounter++;
+		ImGui::Text(name.m_Original.c_str());
+		ImNodes::EndNode();
+	}
+
 	for (Node* node : m_Builder->m_Nodes)
 	{
-		ImNodes::BeginNode(node->m_NodeName.m_Value);
+		ImNodes::BeginNode(idCounter);
+
+		counterMap.emplace(node, idCounter);
+		idCounter++;
+
 		ImNodes::BeginNodeTitleBar();
 		ImGui::Text(node->m_NodeName.m_Original.c_str());
 		ImNodes::EndNodeTitleBar();
 		for (ExecutionSocket* execution : node->m_InputExecutionSockets)
 		{
-			ImNodes::BeginInputAttribute(execution->m_SocketName.m_Value, ImNodesPinShape_Triangle);
+			ImNodes::BeginInputAttribute(idCounter, ImNodesPinShape_Triangle);
+
+			counterMap.emplace(execution, idCounter);
+			idCounter++;
+
 			ImGui::Text(execution->m_SocketName.m_Original.c_str());
 			ImNodes::EndInputAttribute();
 		}
 		for (auto [name, socket] : node->m_InputDataSockets)
 		{
-			ImNodes::BeginInputAttribute(name.m_Value, ImNodesPinShape_QuadFilled);
+			ImNodes::BeginInputAttribute(idCounter, ImNodesPinShape_QuadFilled);
+
+			counterMap.emplace(socket, idCounter);
+			idCounter++;
+
 			ImGui::Text(name.m_Original.c_str());
 			ImNodes::EndInputAttribute();
 		}
 		for (ExecutionSocket* execution : node->m_OutputExecutionSockets)
 		{
-			ImNodes::BeginOutputAttribute(execution->m_SocketName.m_Value, ImNodesPinShape_Triangle);
+			ImNodes::BeginOutputAttribute(idCounter, ImNodesPinShape_Triangle);
+			counterMap.emplace(execution, idCounter);
+			idCounter++;
 			ImGui::Text(execution->m_SocketName.m_Original.c_str());
 			ImNodes::EndOutputAttribute();
 		}
 		for (auto [name, socket] : node->m_OutputDataSockets)
 		{
-			ImNodes::BeginOutputAttribute(name.m_Value, ImNodesPinShape_QuadFilled);
+			ImNodes::BeginOutputAttribute(idCounter, ImNodesPinShape_QuadFilled);
+
+			counterMap.emplace(socket, idCounter);
+			idCounter++;
+
 			ImGui::Text(name.m_Original.c_str());
 			ImNodes::EndOutputAttribute();
 		}
 		ImNodes::EndNode();
+	}
+
+	for (auto& conn : m_Builder->m_ExecutionConnections)
+	{
+		ImNodes::Link(idCounter++, counterMap[conn.m_RHS], counterMap[conn.m_LHS]);
+	}
+
+	for (auto& conn : m_Builder->m_DataConnections)
+	{
+		ImNodes::Link(idCounter++, counterMap[conn->m_RHS], counterMap[conn->m_LHS]);
 	}
 
 	ImNodes::EndNodeEditor();
