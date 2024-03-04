1c464e1e6dddac22ed65e9a9fc3bb1710270444c
tadgem <98434757+tadgem@users.noreply.github.com>
2024-03-04 20:13:26 +0000

    stub raylib module

---
+++ graphscript-raylib/include/GraphScript_Raylib.h
@@ -0,0 +1,11 @@
+#ifndef GRAPHSCRIPT_RAYLIB_H
+#define GRAPHSCRIPT_RAYLIB_H
+
+#include "GraphScript.h"
+
+namespace gs
+{
+	void AddRaylib(Context& c);
+}
+
+#endif
---
+++ graphscript-raylib/src/GraphScript_Raylib.cpp
@@ -0,0 +1,6 @@
+#include "GraphScript_Raylib.h"
+#include "raylib.h"
+
+void gs::AddRaylib(Context& c)
+{
+}
